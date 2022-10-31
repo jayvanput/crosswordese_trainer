3f06acd67d33146fe2a456e102cc210e893a47a9
jvp119 <jayvanput@gmail.com>
2022-10-30 22:02:45 -0400

    Create Word model & migration

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Word(models.Model):
+    word = models.CharField(max_length=21)
+    frequency = models.IntegerField(default=0)
+    ngram_score = models.FloatField(default=0.0)
